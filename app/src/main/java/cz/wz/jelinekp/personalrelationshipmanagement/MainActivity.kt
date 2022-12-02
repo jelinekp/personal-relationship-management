990e5fec5d3a4ae8652f27a02a14381dee9b4455
jelinekp <jelinekp6@gmail.com>
2022-12-02 18:53:31 +0800

    Initial Template generated

---
+++ app/src/main/java/cz/wz/jelinekp/personalrelationshipmanagement/MainActivity.kt
@@ -0,0 +1,39 @@
+package cz.wz.jelinekp.personalrelationshipmanagement
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import cz.wz.jelinekp.personalrelationshipmanagement.ui.PrmApp
+import cz.wz.jelinekp.personalrelationshipmanagement.ui.theme.PersonalRelationshipManagementTheme
+
+class MainActivity : ComponentActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContent {
+			PersonalRelationshipManagementTheme {
+				// A surface container using the 'background' color from the theme
+				Surface(
+					modifier = Modifier.fillMaxSize(),
+					color = MaterialTheme.colorScheme.background
+				) {
+					PrmApp()
+				}
+			}
+		}
+	}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+	PersonalRelationshipManagementTheme {
+		PrmApp()
+	}
+}
---
+++ app/src/main/java/cz/wz/jelinekp/personalrelationshipmanagement/ui/PrmApp.kt
@@ -0,0 +1,7 @@
+package cz.wz.jelinekp.personalrelationshipmanagement.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PrmApp() {
+}
