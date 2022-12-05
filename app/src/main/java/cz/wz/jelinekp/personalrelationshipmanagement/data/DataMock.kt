973f2eb0ad4f9b139628502fc487f87f75dc7d7d
jelinekp <jelinekp6@gmail.com>
2022-12-05 20:44:27 +0800

    Added expandable cards

--- app/src/main/java/cz/wz/jelinekp/personalrelationshipmanagement/data/DataMock.kt
+++ app/src/main/java/cz/wz/jelinekp/personalrelationshipmanagement/data/DataMock.kt
@@ -2,14 +2,20 @@ package cz.wz.jelinekp.personalrelationshipmanagement.data
 
 import java.util.Date
 
-class Contact (
+data class Contact (
 	val name: String,
-	val lastContacted: Date
+	val lastContacted: Date,
+	val country: String
 )
 
 val contacts = listOf<Contact>(
-	Contact("Pavel Jelínek", Date(122, 10, 3)),
-	Contact("Honza Nový", Date(122, 4, 3)),
-	Contact("Pepa Hrdobec", Date(121, 10, 3)),
-	Contact("Aneta Nováková", Date(122, 10, 23)),
+	Contact("Pavel Jelínek", Date(122, 10, 3), "Česko"),
+	Contact("Honza Nový", Date(122, 4, 3), "Austrálie"),
+	Contact("Pepa Hrdobec", Date(121, 10, 3), "Taiwan"),
+	Contact("Aneta Nováková", Date(122, 10, 23), "Somaliland"),
 )
+
+// TODO more advanced localised date, date shortcuts (today, yesterday, days ago)
+fun Date.extractDateForUi() : String {
+	return day.toString() + ". " + month.toString() + ". " + (year + 1900).toString()
+}
--- app/src/main/java/cz/wz/jelinekp/personalrelationshipmanagement/ui/PrmApp.kt
+++ app/src/main/java/cz/wz/jelinekp/personalrelationshipmanagement/ui/PrmApp.kt
@@ -1,59 +1,176 @@
 package cz.wz.jelinekp.personalrelationshipmanagement.ui
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.material3.CardDefaults.cardElevation
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cz.wz.jelinekp.personalrelationshipmanagement.R
 import cz.wz.jelinekp.personalrelationshipmanagement.data.Contact
 import cz.wz.jelinekp.personalrelationshipmanagement.data.contacts
+import cz.wz.jelinekp.personalrelationshipmanagement.data.extractDateForUi
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PrmApp() {
-	Scaffold(topBar = {
-		PrmTopBar()
-	}) { paddingValues ->
-		LazyColumn(
+	Scaffold(
+		topBar = { PrmTopBar()	},
+		floatingActionButtonPosition = FabPosition.End,
+		floatingActionButton = { AddContactFab {
+		
+		} },
+	) { paddingValues ->
+		Column(
 			modifier = Modifier
-				.background(
-					MaterialTheme.colorScheme.background
-				)
-				.padding(16.dp),
-			contentPadding = paddingValues,
-			verticalArrangement = Arrangement.spacedBy(12.dp),
+				.padding(paddingValues)
+				.fillMaxSize()
+		) {
+			Text(
+				text = stringResource(id = R.string.who_to_contact),
+				style = MaterialTheme.typography.headlineSmall,
+				color = MaterialTheme.colorScheme.onBackground,
+				modifier = Modifier
+					.padding(8.dp)
+			)
+			LazyColumn(
+				modifier = Modifier
+					.background(
+						MaterialTheme.colorScheme.background
+					)
+					.padding(horizontal = 16.dp)
+					.fillMaxSize(),
+				verticalArrangement = Arrangement.spacedBy(12.dp),
 			) {
-			items(contacts) {
-				ContactItem(contact = it)
+				items(contacts) {
+					ContactItem(contact = it)
+				}
+			}
+		}
+	}
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ContactItem(
+	contact: Contact,
+	modifier: Modifier = Modifier
+) {
+	var expanded by remember {
+		mutableStateOf(false)
+	}
+	
+	val color by animateColorAsState(targetValue =
+		if (expanded)
+			MaterialTheme.colorScheme.primaryContainer
+		else
+				MaterialTheme.colorScheme.secondaryContainer)
+	
+	Card(
+		modifier = modifier
+			.fillMaxWidth(),
+		elevation = cardElevation(4.dp),
+		onClick = { expanded = !expanded }
+		) {
+		Row(
+			modifier = Modifier
+				.background(color)
+				.animateContentSize(
+					animationSpec = spring(
+						dampingRatio = Spring.DampingRatioLowBouncy,
+						stiffness = Spring.StiffnessMediumLow
+					)
+				)
+		) {
+			Column(
+				modifier = Modifier
+					.weight(9f)
+					.padding(8.dp)
+			) {
+				BasicContactInfo(contact = contact)
+				if (expanded) {
+					MoreContactInfo(contact = contact)
+				}
+			}
+			Column(
+				modifier = Modifier
+					.weight(7f)
+					.padding(4.dp),
+				verticalArrangement = Arrangement.spacedBy(6.dp),
+				horizontalAlignment = Alignment.End
+			) {
+				ConntactedTodayButton()
+				if (expanded) {
+					SetContactedDateButton()
+				}
 			}
 		}
 	}
 }
 
 @Composable
-fun ContactItem(
-	contact: Contact,
+fun BasicContactInfo(
+	contact: Contact
+) {
+	Text(
+		text = contact.name,
+		style = MaterialTheme.typography.bodyLarge
+	)
+	Text(
+		text = contact.lastContacted.extractDateForUi(),
+		style = MaterialTheme.typography.bodyMedium,
+	)
+}
+
+@Composable
+fun MoreContactInfo(
+	contact: Contact
+) {
+	Text(
+		text = stringResource(id = R.string.country, contact.country),
+		style = MaterialTheme.typography.bodyMedium,
+	)
+}
+
+@Composable
+fun ConntactedTodayButton(
 	modifier: Modifier = Modifier
 ) {
-	Card(
-		modifier = modifier,
-		elevation = cardElevation(4.dp),
-		) {
-		Row(
-			modifier = Modifier
-			.fillMaxWidth()
-			.padding(8.dp)
-		) {
-			Text(text = contact.name)
-		}
+	Button(
+		onClick = { /*TODO*/ },
+		modifier = modifier
+	) {
+		Text(
+			text = stringResource(id = R.string.contacted_today),
+			style = MaterialTheme.typography.bodySmall
+		)
+	}
+}
+
+@Composable
+fun SetContactedDateButton(
+	modifier: Modifier = Modifier
+) {
+	Button(
+		onClick = { /*TODO*/ },
+		modifier = modifier
+	) {
+		Text(
+			text = stringResource(id = R.string.set_contacted_date),
+			style = MaterialTheme.typography.bodySmall
+		)
 	}
 }
 
@@ -70,6 +187,19 @@ fun PrmTopBar() {
 			style = MaterialTheme.typography.headlineSmall,
 			modifier = Modifier.padding(horizontal = 6.dp, vertical = 12.dp),
 			color = MaterialTheme.colorScheme.onPrimary
+		)
+	}
+}
+
+@Composable
+fun AddContactFab(
+	onClick: () -> Unit = {}
+) {
+	FloatingActionButton(onClick = onClick){
+		Text(
+			text = "+",
+			fontSize = 36.sp,
+			fontWeight = FontWeight.Light
 		)
 	}
 }
